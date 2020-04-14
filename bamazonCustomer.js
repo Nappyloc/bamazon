cc13d0374afc197aff786a10e0e944b131b2f363
Dania King <dania@fortunebuilders.com>
2020-04-13 23:43:23 -0700

    start function working and returning data-price calculating

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require( 'mysql' )
+var inquirer = require( "inquirer" );
+
 // create the connection information for the sql database
 var connection = mysql.createConnection( {
     host: "localhost",
@@ -20,10 +22,97 @@ connection.connect( function ( err )
 {
     if ( err ) throw err;
     console.log( "connected as id " + connection.threadId + "\n" );
+    startBamazon()
 
 } );
 
 // funciton to start which returns all product information (console.table) Then start Dialoge
+
+function startBamazon ()
+{
+    var query = "SELECT item_id, product_name, price  FROM products";
+    connection.query( query, function ( err, res )
+    {
+
+        console.table( res );
+        inquirer.prompt( [ {
+            name: "productID",
+            type: "input",
+            message: "What is the item_id of the product you would like to purchase?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+        }
+        ] ).then( function ( answer )
+        {
+            var product = answer.productID
+            var quantity = parseInt( answer.quantity )
+            console.log( "requested Quantity", quantity )
+
+            // compare current quantity to requested purchase amount
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query( query, { item_id: product }, function ( err, res )
+            {
+                console.log( res )
+                var curQuant = parseInt( res[ 0 ].stock_quantity )
+                var price = res[ 0 ].price
+                console.log( "this the return ", curQuant, price )
+                if ( curQuant >= quantity )
+                {
+                    completePurchase( curQuant, quantity, price );
+                    connection.end();
+                } else
+                {
+                    console.log( "Insuffiecent Quantity please select another item" )
+                }
+                startBamazon()
+
+            } )
+        } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } )
+
+}
+
+
+var completePurchase = function ( curQuant, quantity, price )
+{
+    // 
+    var total = quantity * price
+    console.log( "your total purchase is $", total )
+
+    // update database with new quantity
+    var newQuant = curQuant - quantity
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 // funciton to start dialogue
 
